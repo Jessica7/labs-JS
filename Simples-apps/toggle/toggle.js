35222cd251fbc552cd9dc3f3cb5646009d96d13c
Jessica <jessica.nascimento6@gmail.com>
2015-02-15 15:04:58 -0300

    toggle com efeito opacity

--- Simples-apps/toggle/toggle.js
+++ Simples-apps/toggle/toggle.js
@@ -1,37 +1,45 @@
-var Toolbar = {
+var Toggle = {
 
-	box: 			document.querySelector('.box'),
-	btn: 		 	document.querySelector('.show-hide'),
-	efeitos:  document.querySelector('.one'),
+	box: 					document.querySelector('.box'),
+	btn: 		 			document.querySelector('.show-hide'),
+	duration: 		1000,
+	steps: 				20,
+	delay: 				5000,
 
 	clickBtn: function(){
 
 		var _this = this;
+
 		this.btn.addEventListener('click', function(){
-				_this.check();
+			Toggle.check();
 		});
+
 	},
 
-	show: function(){
-		this.box.className = 'show';
+	setOpacity: function(el){
+		 this.box.style.opacity = el;
 	},
 
-	hide: function(){
-		this.box.className = 'hide';
+	fadeIn: function(){
+		for(var i = 0; i < 1; i += (1 / this.steps) ){
+			setTimeout("Toggle.setOpacity(" + (  i  ) + ")" , i * Toggle.duration);
+		}
+	},
+
+	fadeOn: function(){
+		for(var i = 0; i < 1; i += (1 / this.steps) ){
+			setTimeout("Toggle.setOpacity(" + ( 1 - i ) + ")" , i * Toggle.duration);
+		}
 	},
 
 	check: function(){
-
-		if(  this.box.className == 'box' ){
-		 		this.hide();
-		}else if( this.box.className == 'hide' ){
-			 	this.show();
+		if(  this.box.style.opacity < 0.95  ){
+	 		this.fadeIn();
 		}else{
-			 	this.hide();	
+	 		this.fadeOn();
 		}
-
 	}
-	
-}
 
-Toolbar.clickBtn();
+}	
+
+Toggle.clickBtn();
