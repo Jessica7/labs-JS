8355a8316779650a37feb18b1664f32836f4225c
jessia <jessica.nascimento6@gmail.com>
2015-03-07 23:11:56 -0300

    slide show v2

--- Simples-apps/slideshow/app.js
+++ Simples-apps/slideshow/app.js
@@ -1,31 +1,51 @@
-
 var SlideShow = (function(){
 
-  var imgs    = ['img/01.jpg','img/02.jpg','img/03.jpg'];
-  var next    = document.getElementById('btn');
-  var box     = document.querySelector('.box img');
-  var count   = 0;
-  
- var setInt = function(){
-  setInterval(function(){
-
-    for(var i = 0; i < imgs.length; i++){
-      console.log(imgs[i]);
-    }
+  var imgs    = ['img/01.jpg','img/02.jpg','img/03.jpg'],
+      prev    = document.getElementById('prev'),
+      next    = document.getElementById('next'),
+      box     = document.querySelector('.box img'),
+      count   = 0;
+           
+  var loopImg = function(){
     count = (count != imgs.length - 1) ? count + 1 : 0;
     box.src = imgs[count];
-
-  }, 3000);   
-    
+    return count;
   };
 
+  var setInt = function(){
+  
+    box.src = imgs[0];
+    setInterval(function(){
+      if(next > 0) setNext();
+      loopImg();
+    }, 3000);
+  
+  };  
+
+  var setNext = function(){
+    next.addEventListener('click', function(){
+      var el = loopImg(); 
+      setPrev(el);
+    });
+  };
+
+  var setPrev = function(atual){
+
+    prev.addEventListener('click', function(){
+      atual--;
+      box.src = imgs[atual];
+      if(atual < 0) loopImg();
+    });
+  };
+
+  var init = function(){
+    setInt();
+    setNext();
+    setPrev();
+  };
+  
   return{
-    setInt: setInt
+    init: init(),
   };
 
 })();
-
-SlideShow.setInt(); 
-
-
-
